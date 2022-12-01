d729e49291af5e9d8053ac4cddd741450e795f97
smohan67 <siddhu.mohan6@gmail.com>
2022-11-30 23:20:47 -0600

    Initial commit

---
+++ SimpleSoundboard/Microphone.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using NAudio.Wave;
+
+namespace SimpleSoundboard
+{
+    public class Microphone
+    {
+        private WaveInEvent _waveIn;
+
+        private WaveOutEvent _waveOut;
+        
+        private BufferedWaveProvider _bufferedWaveProvider; 
+        
+        public Microphone(int microphoneId, int outputId)
+        {
+            //initalize wave in and wave out devices
+            _waveIn = new WaveInEvent();
+            _waveIn.DeviceNumber = microphoneId;
+            _waveOut = new WaveOutEvent();
+            _waveOut.DeviceNumber = outputId;
+            
+            //initalize wave out data
+            _bufferedWaveProvider = new BufferedWaveProvider(_waveIn.WaveFormat);
+            _waveOut.Init(_bufferedWaveProvider);
+            _waveOut.Play();
+            
+            //start recording
+            _waveIn.StartRecording();
+            _waveIn.DataAvailable += OnDataAvailable;
+
+            //run main loop on another thread
+            Thread thread = new Thread(MainLoop);
+            thread.Start();
+        }
+
+        public void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            _bufferedWaveProvider.AddSamples(e.Buffer, 0 , e.BytesRecorded);
+        }
+        
+        public void MainLoop()
+        {
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
---
+++ SimpleSoundboard/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace SimpleSoundboard
+{
+    internal class Program
+    {
+        private static WaveOut _SoundEffectWaveOut;
+
+        private static int VB_OUTPUT = 3;
+        private static int MIC_INPUT = 1;
+        
+        
+        public static void Main(string[] args)
+        {
+           Microphone m = new Microphone(MIC_INPUT, VB_OUTPUT);
+
+           _SoundEffectWaveOut = new WaveOut();
+           _SoundEffectWaveOut.DeviceNumber = VB_OUTPUT;
+           
+           
+           Console.WriteLine("----------WaveIn Devices:-------------------");
+           for (int n = -1; n < WaveIn.DeviceCount; n++)
+           {
+               var caps = WaveIn.GetCapabilities(n);
+               Console.WriteLine($"{n}: {caps.ProductName}");
+           }
+            
+           Console.WriteLine("----------WaveOut Devices:-------------------");
+           for (int n = -1; n < WaveOut.DeviceCount; n++)
+           {
+               var caps = WaveOut.GetCapabilities(n);
+               Console.WriteLine($"{n}: {caps.ProductName}");
+           }
+           
+           while (true)
+           {
+                
+               var input = Console.ReadLine();
+               //quit if q is being pressed
+               if (input.Equals("q"))
+               {
+                   break;
+               }
+               PlaySoundboard();
+
+           }
+        }
+        /*
+        public static void PlaySoundboard()
+        {
+            Console.WriteLine("Playing soundboard");            
+            //set device number
+            int deviceNumber = 3;
+            
+            //load in audio file
+            var audioFile = new NAudio.Wave.AudioFileReader(@"C:\Users\siddh\Desktop\vine-boom.mp3");
+            
+            //create and set up new waveout device with audio file
+            var waveOut = new NAudio.Wave.WaveOut();
+            waveOut.DeviceNumber = deviceNumber;
+            waveOut.Init(audioFile);
+            
+            //play audio file
+            waveOut.Play();
+            //dispose resources
+            audioFile.Dispose();
+            waveOut.Dispose();
+        }
+        */
+        
+        
+        public static void PlaySoundboard()
+        {
+            Console.WriteLine("Playing soundboard");
+            //set device number
+            
+            
+            int deviceNumber = 3;
+            var audioFile = new NAudio.Wave.AudioFileReader(@"C:\Users\siddh\Desktop\vine-boom.mp3");
+            //var waveOut = new NAudio.Wave.WaveOut();
+            //waveOut.DeviceNumber = deviceNumber;
+
+            _SoundEffectWaveOut.Init(audioFile);
+            _SoundEffectWaveOut.Play();
+            
+            
+            
+            //Disposing waveout breaks this????
+            //disposing audiofile makes sound play twice???
+            
+            //audioFile.Dispose();
+            //_SoundEffectWaveOut.Dispose();
+        }
+        
+    }
+}
